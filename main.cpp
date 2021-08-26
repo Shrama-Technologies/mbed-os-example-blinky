b4a1d2563410ec4ab2c4414e0a8d11466706e122
st-debdeep <debdeep@shrama.in>
2021-08-26 20:31:28 +0530

    Tweak the app little bit for vertexcom platform

--- main.cpp
+++ main.cpp
@@ -7,16 +7,37 @@
 
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+#define BLINKING_RATE     500
 
 
 int main()
 {
+	int count = 0;
+
     // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+    DigitalOut led1(LED1);
+    DigitalOut led0(LED0);
 
     while (true) {
-        led = !led;
+		count++;
+		if (count < 20) {
+			led1 = 1;
+			led0 = !led0;
+		} else if (count < 40) {
+			led0 = 1;
+			led1 = !led1;
+		} else if (count < 60) {
+			led0 = !led0;
+			if (led0) {
+				led1 = !led1;
+			}
+		} else if (count < 80){
+			led0 = !led0;
+			led1 = !led1;
+		} else {
+			count = 0;
+		}
+
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
